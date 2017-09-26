a61d116d0135a35df5557358e3176c83cd59ebc6
Kevin Naquin <kevin.naquin@digitalglobe.com>
2017-09-26 16:42:48 -0400

    Update Time_Lapse_Viewer_UI_StepDef.groovy

--- src/main/groovy/ossim/cucumber/step_definitions/Time_Lapse_Viewer_UI_StepDef.groovy
+++ src/main/groovy/ossim/cucumber/step_definitions/Time_Lapse_Viewer_UI_StepDef.groovy
@@ -74,10 +74,10 @@ And(~/I add a (.*) annotation$/) {
             case "polygon":
                 def x1 = 0 //random.nextInt(xMax - xMin) + xMin
                 def y1 = 0 //random.nextInt(yMax - yMin) + yMin
-                def x2 = 10 //random.nextInt(xMax - xMin) + xMin
-                def y2 = 10 //random.nextInt(yMax - yMin) + yMin
-                def x3 = 10 //random.nextInt(xMax - xMin) + xMin
-                def y3 = 10 //random.nextInt(yMax - yMin) + yMin
+                def x2 = 20 //random.nextInt(xMax - xMin) + xMin
+                def y2 = 20 //random.nextInt(yMax - yMin) + yMin
+                def x3 = 20 //random.nextInt(xMax - xMin) + xMin
+                def y3 = 20 //random.nextInt(yMax - yMin) + yMin
                 //def x4 = 5 //random.nextInt(xMax - xMin) + xMin
                 //def y4 = 5 //random.nextInt(yMax - yMin) + yMin
                 //def x5 = 5 //random.nextInt(xMax - xMin) + xMin
