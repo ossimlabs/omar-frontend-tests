f5f18837771a94bbd5cb5a85f03e1090c4d97f3a
Kevin Naquin <kevin.naquin@digitalglobe.com>
2017-09-26 15:12:04 -0400

    Update Time_Lapse_Viewer_UI_StepDef.groovy

--- src/main/groovy/ossim/cucumber/step_definitions/Time_Lapse_Viewer_UI_StepDef.groovy
+++ src/main/groovy/ossim/cucumber/step_definitions/Time_Lapse_Viewer_UI_StepDef.groovy
@@ -49,11 +49,11 @@ And(~/I add a (.*) annotation$/) {
 
         def map = browser.page.$("#map")
         def random = new Random()
-        def startX = 100 //map.width / 2 as Integer
-        def startY = 170 //map.height / 2 as Integer
-        def xMax = 150 //map.width
+        def startX = map.width / 2 as Integer
+        def startY = map.height / 2 as Integer
+        def xMax = map.width
         def xMin = 50
-        def yMax = 220 //map.height
+        def yMax = map.height
         def yMin = 120
 
         def actions = new Actions(browser.driver)
@@ -62,32 +62,32 @@ And(~/I add a (.*) annotation$/) {
             case "circle":
             case "rectangle":
             case "square":
-                def x1 = random.nextInt(xMax - xMin) + xMin
-                def y1 = random.nextInt(yMax - yMin) + yMin
-                def x2 = random.nextInt(xMax - xMin) + xMin
-                def y2 = random.nextInt(yMax - yMin) + yMin
+                def x1 = 10 //random.nextInt(xMax - xMin) + xMin
+                def y1 = 10 //random.nextInt(yMax - yMin) + yMin
+                def x2 = 10 //random.nextInt(xMax - xMin) + xMin
+                def y2 = 10 //random.nextInt(yMax - yMin) + yMin
                 actions.moveToElement(map.firstElement()).moveByOffset(-startX, -startY).moveByOffset(x1, y1).click().moveByOffset(x2 - x1, y2 - y1).click().perform()
                 sleep(1000)
                 break
             case "line":
             case "polygon":
-                def x1 = random.nextInt(xMax - xMin) + xMin
-                def y1 = random.nextInt(yMax - yMin) + yMin
-                def x2 = random.nextInt(xMax - xMin) + xMin
-                def y2 = random.nextInt(yMax - yMin) + yMin
-                def x3 = random.nextInt(xMax - xMin) + xMin
-                def y3 = random.nextInt(yMax - yMin) + yMin
-                def x4 = random.nextInt(xMax - xMin) + xMin
-                def y4 = random.nextInt(yMax - yMin) + yMin
-                def x5 = random.nextInt(xMax - xMin) + xMin
-                def y5 = random.nextInt(yMax - yMin) + yMin
+                def x1 = 5 //random.nextInt(xMax - xMin) + xMin
+                def y1 = 5 //random.nextInt(yMax - yMin) + yMin
+                def x2 = 5 random.nextInt(xMax - xMin) + xMin
+                def y2 = 5 random.nextInt(yMax - yMin) + yMin
+                def x3 = 5 //random.nextInt(xMax - xMin) + xMin
+                def y3 = 5 //random.nextInt(yMax - yMin) + yMin
+                def x4 = 5 //random.nextInt(xMax - xMin) + xMin
+                def y4 = 5 //random.nextInt(yMax - yMin) + yMin
+                def x5 = 5 //random.nextInt(xMax - xMin) + xMin
+                def y5 = 5 //random.nextInt(yMax - yMin) + yMin
                 actions.moveToElement(map.firstElement()).moveByOffset(-startX, -startY).moveByOffset(x1, y1).click().moveByOffset(x2 - x1, y2 - y1).click().moveByOffset(x3 - x2, y3 - y2).click().moveByOffset(x4 - x3, y4 - y3).click().moveByOffset(x5 - x4, y5 - y4).click().perform()
                 browser.driver.executeScript("return tlv.drawAnnotationInteraction.finishDrawing();")
                 sleep(1000)
                 break
             case "point":
-                def x1 = random.nextInt(xMax - xMin) + xMin
-                def y1 = random.nextInt(yMax - yMin) + yMin
+                def x1 = 10 //random.nextInt(xMax - xMin) + xMin
+                def y1 = 10 //random.nextInt(yMax - yMin) + yMin
                 actions.moveToElement(map.firstElement()).moveByOffset(-startX, -startY).moveByOffset(x1, y1).click().perform()
                 sleep(1000)
                 break
