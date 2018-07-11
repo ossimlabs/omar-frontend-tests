5035ad1d8ec553e4337a27bcc16c07aa161426ce
oscarkramer <oscar.kramer@radiantsolutions.com>
2018-07-11 10:59:38 -0400

    Added multiple target profiels. Now more closely mirrors S3 copy.

--- src/cucumber/resources/cucumber-config.groovy
+++ src/cucumber/resources/cucumber-config.groovy
@@ -1,4 +1,24 @@
-rbtcloudRootDir = "https://omar-dev.ossim.io"
+rbtcloudRootDir = "NOT_ASSIGNED"
+targetDeployment = System.getenv("TEST_PROFILE")
+switch(targetDeployment) {
+   case "dev":
+      rbtcloudRootDir = "https://omar-dev.ossim.io"
+      break
+   case "stage":
+      rbtcloudRootDir = "https://omar-stage.ossim.io"
+      break
+   case "prod":
+      rbtcloudRootDir = "https://omar-prod.ossim.io"
+      break
+   case "rel":
+      rbtcloudRootDir = "https://omar-rel.ossim.io"
+      break
+   default:
+      rbtcloudRootDir = "https://omar-dev.ossim.io"
+      break
+}
+
+println("\nOMAR URL being tested: ${rbtcloudRootDir}\n")
 
 tlvUrl = "${rbtcloudRootDir}/tlv"
 mensaUrl = "${rbtcloudRootDir}/omar-mensa"
