3ca2ee1e2c1f93790d30cd043060e6ef0018fae4
dccohee <calecohee@gmail.com>
2019-09-27 13:41:59 -0400

    Start and stop selenium/browser

--- src/cucumber/resources/features/05_O2_PKI.feature
+++ src/cucumber/resources/features/05_O2_PKI.feature
@@ -2,15 +2,18 @@
 Feature: O2_PKI
 
     Scenario: Start Selenium
-      Given I am starting the omar pki ui selenium server
+      Given I am starting the selenium server
 
     Scenario: Start browser
-      Given I am creating the omar pki browser
+      Given I am starting the browser
 
-    Scenario: Missing PKI
-      Given that I try to enter the omar pki home page using Firefox
-      When I attempt to log in without a pki
-      Then it does not let me into omar
+#    Scenario: Missing PKI
+#      Given that I try to enter the omar pki home page using Firefox
+#      When I attempt to log in without a pki
+#      Then it does not let me into omar
 
     Scenario: Stop Selenium
       Given I am stopping the selenium server
+
+    Scenario: Stop browser
+      Given I am stopping the browser
--- src/main/groovy/ossim/cucumber/step_definitions/O2_PKI_StepDef.groovy
+++ src/main/groovy/ossim/cucumber/step_definitions/O2_PKI_StepDef.groovy
@@ -1,25 +1,50 @@
 package ossim.cucumber.step_definitions
 
-Given(~/^I am starting the omar pki ui selenium server$/) {
+import geb.Browser
+import org.openqa.selenium.firefox.FirefoxDriver
+import ossim.cucumber.config.CucumberConfig
+
+this.metaClass.mixin(cucumber.api.groovy.Hooks)
+this.metaClass.mixin(cucumber.api.groovy.EN)
+
+def config = CucumberConfig.config
+def homePageUrl = config.pkiUrl
+remoteDisplay = null
+
+Given(~/^I am starting the selenium server$/) { ->
+    println "Starting browser..."
+    def command = ["Xvfb", ":1", "-screen", "0", "1366x768x24", "-ac"]
+    remoteDisplay = command.execute()
+    sleep(3000)
+}
+
+Given(~/^I am starting the browser$/) { ->
+    println "Starting browser..."
+    browser = new Browser(driver: new FirefoxDriver())
+    sleep(3000)
+}
+
+Given(~/^I am stopping the selenium server$/) { ->
+    println "Stopping browser"
+    remoteDisplay.waitForOrKill(1)
+}
+
+Given(~/^I am stopping the browser$/) { ->
+    println "Stopping browser"
+    browser.quit()
+}
+
+/*
+Given(~/^that I try to enter the omar pki home page using Firefox$/) { ->
 
 }
 
-Given(~/^I am creating the omar pki browser$/) {
+When(~/^I attempt to log in without a pki$/) { ->
 
 }
 
-Given(~/^that I try to enter the omar pki home page using Firefox$/) {
+Then(~/^it does not let me into the omar$/) { ->
 
 }
+ */
 
-When(~/^I attempt to log in without a pki$/) {
-
-}
-
-Then(~/^it does not let me into the omar$/) {
-
-}
-
-Given(~/^I am stopping the selenium server$/) {
-
-}
