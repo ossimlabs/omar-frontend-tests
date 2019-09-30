f8e72843231cd81cfd7bb23d9226fb6b5531f467
Cale Cohee <calecohee@gmail.com>
2019-09-30 16:07:44 -0400

    Update O2_PKI_StepDef.groovy

--- src/main/groovy/ossim/cucumber/step_definitions/O2_PKI_StepDef.groovy
+++ src/main/groovy/ossim/cucumber/step_definitions/O2_PKI_StepDef.groovy
@@ -8,7 +8,6 @@ this.metaClass.mixin(cucumber.api.groovy.Hooks)
 this.metaClass.mixin(cucumber.api.groovy.EN)
 
 def pkiHomePageUrl = config.pkiUrl
-def pkiHomePageUrl = "https://pki-omar-dev.ossim.io"
 remoteDisplay = null
 browser = null
 
